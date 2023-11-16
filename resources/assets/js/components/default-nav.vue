267f8d262cdc877439ca40703f3d2b01f2afa289
Esmeralda Acevedo <esme@ortussolutions.com>
2023-11-16 05:35:30 -0800

    Added type to button.

--- resources/assets/js/components/default-nav.vue
+++ resources/assets/js/components/default-nav.vue
@@ -1,11 +1,11 @@
 <template>
-    <button @click="prev" class="carousel__prev">
+    <button @click="prev" class="carousel__prev" type="button">
       <svg class="carousel__icon" viewBox="0 0 24 24" role="img" aria-label="Arrow pointing to the left">
           <title>Arrow pointing to the left</title>
           <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"></path>
       </svg>
     </button>
-    <button @click="next" class="carousel__next">
+    <button @click="next" class="carousel__next" type="button">
       <svg class="carousel__icon" viewBox="0 0 24 24" role="img" aria-label="Arrow pointing to the right">
             <title>Arrow pointing to the right</title>
             <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"></path>
